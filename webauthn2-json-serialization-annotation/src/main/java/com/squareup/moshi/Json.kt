8ddbe7b327ab828f4a74ca5f5cb0089d5f8e3902
RyuNen344 <s1100633@outlook.com>
2024-12-20 04:44:51 +0900

    fix: do not export shadow annotations (#270)

--- webauthn2-json-serialization-annotation/src/main/java/com/squareup/moshi/Json.kt
+++ webauthn2-json-serialization-annotation/src/main/java/com/squareup/moshi/Json.kt
@@ -3,5 +3,5 @@ package com.squareup.moshi
 /**
  * shadow annotation of [com.squareup.moshi.Json]
  */
-@Retention(AnnotationRetention.RUNTIME)
+@Retention(AnnotationRetention.SOURCE)
 annotation class Json(val name: String = "\u0000", val ignore: Boolean = false)
